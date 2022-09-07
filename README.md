0ec98f843bf73fd36797ea3f69bf9608cef49a09
Lincoln <lincoln.reboucas40@gmail.com>
2022-09-07 19:52:38 -0300

    one more commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # imersaoDev-conversorDeMoedas
+Repositório feito com a Imersão Dev da Alura.
--- javascript.js
+++ javascript.js
@@ -3,16 +3,10 @@ function Converter() {
 	var valorDolar = real * 0.19;
 	var valorEuro = real * 0.19;
 	var valorBitcoin = real * 0.0000098;
-	var dolarConvertido = document.getElementById("dolar");
-	var euroConvertido = document.getElementById("euro");
-	var bitcoinConvertido = document.getElementById("bitcoin");
-	var dolar = "O resultado em dólar é U$ " + valorDolar;
-	var euro = "O resultado em euro é € " + valorEuro;
-	var bitcoin = "O resultado em bitcoin é BTC " + valorBitcoin;
 	
-	dolarConvertido.innerHTML = dolar;
-	euroConvertido.innerHTML = euro;
-	bitcoinConvertido.innerHTML = bitcoin;
+	document.getElementById("dolar").innerHTML = "O resultado em dólar é U$ " + valorDolar;
+	document.getElementById("euro").innerHTML = "O resultado em euro é € " + valorEuro;
+	document.getElementById("bitcoin").innerHTML = "O resultado em bitcoin é BTC " + valorBitcoin;
 
 	// console.log("Valor em real: R$ " + valorReal);
 	// console.log("Valor em dolar: U$ " + valorDolar);
