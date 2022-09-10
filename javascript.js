79fd54f347303bce95f6b2e8ef57d0cb049f1c13
Lincoln <lincoln.reboucas40@gmail.com>
2022-09-10 17:41:08 -0300

    one more change

--- javascript.js
+++ javascript.js
@@ -1,13 +1,26 @@
-function Converter() {
+function converteDolar() {
 	var real = parseFloat(document.getElementById("valor").value);
-	var valorDolar = real * 0.19;
-	var valorEuro = real * 0.19;
-	var valorBitcoin = real * 0.0000098;
-	
-	document.getElementById("dolar").innerHTML = "O resultado em dólar é U$ " + valorDolar;
-	document.getElementById("euro").innerHTML = "O resultado em euro é € " + valorEuro;
-	document.getElementById("bitcoin").innerHTML = "O resultado em bitcoin é BTC " + valorBitcoin;
+	if(real >= 0){
+		document.getElementById("dolar").innerHTML = "O resultado em dólar é U$ " + (real * 0.1943).toFixed(2);
+		document.getElementById("euro").innerHTML = "";
+		document.getElementById("bitcoin").innerHTML = "";
+	}
+}
 
-	// console.log("Valor em real: R$ " + valorReal);
-	// console.log("Valor em dolar: U$ " + valorDolar);
+function converteEuro() {
+	var real = parseFloat(document.getElementById("valor").value);
+	if(real >= 0){
+		document.getElementById("dolar").innerHTML = "";
+		document.getElementById("euro").innerHTML = "O resultado em euro é € " + (real * 0.1913).toFixed(2);
+		document.getElementById("bitcoin").innerHTML = "";
+	}
+}
+
+function converteBitcoin() {
+	var real = parseFloat(document.getElementById("valor").value);
+	if(real >= 0){
+		document.getElementById("dolar").innerHTML = "";
+		document.getElementById("euro").innerHTML = "";
+		document.getElementById("bitcoin").innerHTML = "O resultado em bitcoin é BTC " + (real * 0.0000098);
+	}
 }
